4694a29a949b404162e5b4f010b8e2b7d96e644d
Erick Lopez <erick83@gmail.com>
2018-01-10 22:05:56 -0400

    mend

--- truthcheck.js
+++ truthcheck.js
@@ -9,11 +9,15 @@
 function truthCheck(collection, pre) {
   // Is everyone being true?
   for (var i = 0; i < collection.length; i++) {
-    if (collection[i][pre] === undefined || collection[i][pre]) {
+    if (!isValid(collection[i][pre])) {
       return false;
     }
   }
   return true;
 }
+
+var isValid = function (val) {
+  return !(!val || /^[\s-0]*$/.test(val));
+};
 
 truthCheck([{"user": "Tinky-Winky", "sex": "male"}, {"user": "Dipsy", "sex": "male"}, {"user": "Laa-Laa", "sex": "female"}, {"user": "Po", "sex": "female"}], "sex");
